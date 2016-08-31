317d561b2f977dd9e12a731ed309cb4eafd2dd62
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2016-08-31 18:19:42 -0400

    Refactor users authentication code

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,12 +1,10 @@
 # http://www.rubydoc.info/github/plataformatec/devise/Devise/RegistrationsController
 class Users::RegistrationsController < Devise::RegistrationsController
-# before_action :configure_sign_up_params, only: [:create]
-# before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   def new
     build_resource({})
-    respond_with resource, location: "users/sign_up"
+    redirect_to "users/sign_up"
   end
 
   # POST /resource
@@ -15,35 +13,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
       user.update_columns(confirmed_at: nil)
       user.send_confirmation_instructions
       flash[:info] = "We sent a link to sign in. Please check your inbox."
-      respond_with resource, location: root_url
+      redirect_to root_url
     else
       super
     end
   end
-
-  # GET /resource/edit
-  # def edit
-  #   super
-  # end
-
-  # PUT /resource
-  # def update
-  #   super
-  # end
-
-  # DELETE /resource
-  # def destroy
-  #   super
-  # end
-
-  # GET /resource/cancel
-  # Forces the session data which is usually expired after sign
-  # in to be expired now. This is useful if the user wants to
-  # cancel oauth signing in/up in the middle of the process,
-  # removing all OAuth session data.
-  # def cancel
-  #   super
-  # end
 
   protected
 
@@ -57,24 +31,4 @@ class Users::RegistrationsController < Devise::RegistrationsController
     def update_resource(resource, params)
       resource.update_without_password(params)
     end
-
-    # If you have extra params to permit, append them to the sanitizer.
-    # def configure_sign_up_params
-    #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-    # end
-
-    # If you have extra params to permit, append them to the sanitizer.
-    # def configure_account_update_params
-    #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-    # end
-
-    # The path used after sign up.
-    # def after_sign_up_path_for(resource)
-    #   super(resource)
-    # end
-
-    # The path used after sign up for inactive accounts.
-    # def after_inactive_sign_up_path_for(resource)
-    #   super(resource)
-    # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,23 +2,21 @@ Rails.application.routes.draw do
   root to: 'static_pages#home'
 
   devise_for :users, controllers: {
-    sessions:           "users/sessions",
-    passwords:          "users/passwords",
     registrations:      "users/registrations",
     confirmations:      "users/confirmations",
     omniauth_callbacks: "users/omniauth_callbacks"
   }
   devise_for :clients, controllers: {
-    sessions:           "clients/sessions",
-    passwords:          "clients/passwords",
-    registrations:      "clients/registrations",
-    confirmations:      "clients/confirmations",
+    # sessions:           "clients/sessions",
+    # passwords:          "clients/passwords",
+    # registrations:      "clients/registrations",
+    # confirmations:      "clients/confirmations",
   }
   devise_for :account_executives, controllers: {
-    sessions:           "account_executives/sessions"
+    # sessions:           "account_executives/sessions"
   }
   devise_for :admins, controllers: {
-    sessions:           "admins/sessions"
+    # sessions:           "admins/sessions"
   }
 
   # Ask for email address after successful OAuth.
